aef8ec8495b897cf8a399f3301f89e593bd86a6b
areiz <areiz@github.com>
2018-07-03 18:11:14 -0500

    more efficient fibo

--- src/cracking_practice/fibonacci_sequences.java
+++ src/cracking_practice/fibonacci_sequences.java
@@ -2,13 +2,22 @@ package cracking_practice;
 
 public class fibonacci_sequences {
 
-	static int fib(int n) {
-		if (n <= 1) return n;
-		return fib(n-1) + fib(n-2);
+	static int fibonacci(int n) {
+		if (n == 0) return 0;
+		int a = 0;
+		int b = 1;
+		for (int i = 2; i < n; i++) {
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+		return a + b;
+		
 	}
 	
 	public static void main(String[] args) {
 		int n = 9;
-		System.out.println(fib(n));
+		System.out.println(fibonacci(n));
 	}
+	
 }
