5d1affba7b2ab9e52bfbc563a585cf149cb28a24
areiz <areiz@github.com>
2018-07-03 23:04:05 -0500

    memo addition to countWays

--- src/cracking_practice/tripleStep.java
+++ src/cracking_practice/tripleStep.java
@@ -1,17 +1,29 @@
 package cracking_practice;
 
+import java.util.Arrays;
+
 public class tripleStep {
 
-	static int jump(int n) {
-		if (n < 0) return 0;
-		else if (n == 0) return 1;
-		else {
-			return jump(n - 1) + jump(n - 2) + jump(n - 3);
-		}
-}
+	static int countWays(int n) {
+		int[] memo = new int[n + 1];
+		Arrays.fill(memo, -1);
+		return countWays(n, memo);
+	}
 	
+	static int countWays(int n, int[] memo) {
+		if (n < 0) {
+			return 0;
+		} else if (n == 0){
+			return 1;
+		} else if(memo[n] > -1) {
+			return memo[n];
+		} else {
+			memo[n] = countWays(n - 1, memo) + countWays(n - 2, memo) + countWays(n - 3, memo);
+		}
+		return memo[n];
+	}
 	public static void main(String[] args) {
 		int n = 3;
-		System.out.println(jump(n));
+		System.out.println(countWays(n));
 	}
 }
