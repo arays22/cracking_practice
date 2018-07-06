6400ed639344fadaed0019dcd26e25f37d4d15ad
areiz <areiz@github.com>
2018-07-05 19:07:06 -0500

    robot grid

--- src/cracking_practice/tripleStep.java
+++ src/cracking_practice/tripleStep.java
@@ -13,9 +13,9 @@ public class tripleStep {
 	static int countWays(int n, int[] memo) {
 		if (n < 0) {
 			return 0;
-		} else if (n == 0){
+		} else if (n == 0) {
 			return 1;
-		} else if(memo[n] > -1) {
+		} else if (memo[n] > -1) {
 			return memo[n];
 		} else {
 			memo[n] = countWays(n - 1, memo) + countWays(n - 2, memo) + countWays(n - 3, memo);
