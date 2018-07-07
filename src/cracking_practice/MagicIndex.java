8434666e07e6a1de1847b2b10be1e73892cfbee8
areiz <reiza6@gmail.com>
2018-07-06 19:11:02 -0500

    non-distinct value solution

--- src/cracking_practice/MagicIndex.java
+++ src/cracking_practice/MagicIndex.java
@@ -10,15 +10,24 @@ public class MagicIndex {
 		if (end < start) {
 			return -1;
 		}
-		int mid = (start + end) / 2;
-		if (array[mid] == mid) {
-			return mid;
-		} else if (array[mid] > mid) {
-			return magicFast(array, start, mid - 1);
-		} else {
-			return magicFast(array, mid + 1, end);
+		int midIndex = (start + end) / 2;
+		int midValue = array[midIndex];
+		if (midValue == midIndex) {
+			return midIndex;
 		}
+		
+		int leftIndex = Math.min(midIndex - 1, midValue);
+		int left = magicFast(array, start, leftIndex);
+		if (left >= 0) {
+			return left;
+		}
+		
+		int rightIndex = Math.max(midIndex + 1, midValue);
+		int right = magicFast(array, rightIndex, end);
+		
+		return right;
 	}
+	
 	
 	public static void main(String[] args) {
 		int[] intArray = new int[]{ 0, 3, 4, 5, 6, 7, 8 };
