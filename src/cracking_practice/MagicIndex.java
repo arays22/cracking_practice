7a743dc7599fa6238ae76b864cd0abed72f53b4a
areiz <reiza6@gmail.com>
2018-07-06 19:04:56 -0500

    binary search magic index

--- src/cracking_practice/MagicIndex.java
+++ src/cracking_practice/MagicIndex.java
@@ -2,17 +2,27 @@ package cracking_practice;
 
 public class MagicIndex {
 
-	static int findMagicIndex(int[] array) {
-		for (int i = 0; i < array.length; i++) {
-			int target = array[i];
-			if (target == i) return i;
+	static int magicFast(int[] array) {
+		return magicFast(array, 0, array.length - 1);
+	}
+	
+	static int magicFast(int[] array, int start, int end) {
+		if (end < start) {
+			return -1;
 		}
-		return -1;
+		int mid = (start + end) / 2;
+		if (array[mid] == mid) {
+			return mid;
+		} else if (array[mid] > mid) {
+			return magicFast(array, start, mid - 1);
+		} else {
+			return magicFast(array, mid + 1, end);
+		}
 	}
 	
 	public static void main(String[] args) {
 		int[] intArray = new int[]{ 0, 3, 4, 5, 6, 7, 8 };
-		System.out.println(findMagicIndex(intArray));
+		System.out.println(magicFast(intArray));
 	}
 	
 }
