8157a8de9a9b3395d3626741d9691c47079cb0ea
areiz <reiza6@gmail.com>
2018-07-20 13:04:48 -0500

    body and title parsing

--- src/cracking_practice/piToTheNth.java
+++ src/cracking_practice/piToTheNth.java
@@ -1,5 +1,28 @@
 package cracking_practice;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class piToTheNth {
 
-}
+	
+	
+
+	      public static void main(String[] args) throws IOException {
+
+	    	  Document doc = Jsoup.connect("https://ftw.usatoday.com/2017/03/pi-day-10000-digits-explanation-how-to-understand").get();
+
+	          //String htmlString = "<html><head><title>My title</title></head>"
+	                  //+ "<body>Body content</body></html>";
+
+	          // Document doc = Jsoup.parse(htmlString);
+	          String title = doc.title();
+	          String body = doc.body().text();
+
+	          System.out.printf("Title: %s%n", title);
+	          System.out.printf("Body: %s", body);
+	      }
+	  }
+
